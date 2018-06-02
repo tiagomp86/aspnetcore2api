c3eba5f5b8d93e51176e7f87a9e42f3d6fe42f81
Tiago Muller Pereira <tiagomp986@gmail.com>
2018-06-01 22:41:31 -0300

    Photo Upload

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,7 @@ namespace DatingApp.API
             services.AddMvc();
             services.AddTransient<Seed>();
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
